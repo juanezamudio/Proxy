daa045a47a74bd18fcd4ebf9ce0ba233b2982533
Juan <juan.zamudio@pomona.edu>
2018-04-18 16:07:46 -0700

    ls

--- proxylab-handout/proxy.c
+++ proxylab-handout/proxy.c
@@ -2,6 +2,7 @@
  * A proxy server that connects with a web server and browser
  *
  * Juan Zamudio - jzamudio
+ * Cleo Foreman - cforeman
  *
  * Proxy Port #: 45806
  * Tiny Server Port #: 45807
@@ -15,6 +16,7 @@
 #define MAX_OBJECT_SIZE 102400
 #define PROXY_PORT 45806
 #define SERVER_PORT 45807
+#define PROXY_HTTP "HTTP/1.0"
 
 /* You won't lose style points for including this long line in your code */
 static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
@@ -44,6 +46,14 @@ void parseURL(char* url) {
   sscanf(address, "%[^/]s", hostname);
   sscanf(path, "%s ", query);
   printf("Hostname: %s\n\nQuery: %s\n\n", hostname, query);
+}
+
+void GET() {
+
+}
+
+void POST() {
+  
 }
 
 int main() {
